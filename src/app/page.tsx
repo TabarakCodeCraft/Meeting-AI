7a65b3daa1609d6f3080b30e41b65d56fc630b37
TabarakCodeCraft <tabarak.ali1801a@sc.uobaghdad.edu.iq>
2025-12-03 02:14:26 +0300

    add db neon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Alert } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
